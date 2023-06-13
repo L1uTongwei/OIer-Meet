941d647ae9f9f32562686205220cc0bf188331dd
任宇航 <15543677192@163.com>
2023-06-13 19:34:14 +0800

    update new files

---
+++ website/js/css_style.js
@@ -0,0 +1,59 @@
+const page = `
+    margin-right: .5em;
+    padding: 0;
+    cursor: pointer;
+    width: 1.7em;
+    min-width: 1.7em;
+    height: 1.7em;
+    background: #fff;
+    border: 1px solid #bfbfbf;
+    border-radius: 6px;`; //CSS 页码样式
+const page_img = `
+    overflow-clip-margin: content-box;
+    overflow: clip;
+    border-style: none;
+    display: block;
+    margin: auto;`; //CSS 页码图片样式
+const avatar_css = `
+    vertical-align:middle;
+    margin-left: .4em;
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;`; //CSS 头像样式
+const editor_css = `
+    height: 0px;
+    `;
+const katex_config = {
+    delimiters: [
+        { left: "$$", right: "$$", display: true },
+        { left: "$", right: "$", display: false }
+    ]
+}; //KaTeX 配置
+const about = "### 简介\n\n" +
+    "OIer-Meet 是一套前端、后端集合的程序套装，基于[GNU通用公共许可证 v3.0](https://www.gnu.org/licenses/gpl-3.0.zh-cn.html)开源。\n\n" +
+    "欢迎加入我们的团队。您若有此意向，请联系 QQ：819433558 / 1347277058。\n\n" +
+    "使用本软件意味着你已经同意[OIer-Meet最终用户许可协议](https://gitee.com/organizations/oier-meet-dev-team/cla/eula)。\n\n" +
+    "项目地址：https://gitee.com/oier-meet-dev-team/oier-meet。\n\n" +
+    "本站为前后端分离，建议将本站下载下来，以防 Netlify 服务出现问题。\n\n" +
+    "OIer-Meet Web Frontend ©2022 OIer-Meet Dev \n\n";
+const words = [
+    '如果感到累的话，不努力也是可以的。',
+    '不要想着说对自己狠一点。自我冲突，自我对抗只能使情形越来越糟糕。',
+    '够了，你做的够多了。不用那么努力地想要好起来也没问题。',
+    '这不是你的错。这不是你的错。这不是你的错。',
+    '但是啊，会好起来的，你也会的。',
+    '找些事情做，转移注意力，例如散步、下棋、骑脚踏车、阅读等。',
+    '凡事只求尽力，不要完美主义，因为很多结果的呈现，并非自己可以决定。',
+    '你还是原来的那个你，只是需要一段时间重整旗鼓而已。',
+    '希望你更爱自己，更爱世界。',
+    '没什么的，天总会亮的，总有一天你又会精神奕奕的。',
+    '没事，我在的。没关系，我陪着你。',
+    '一定也要照顾好自己',
+    '不谈未来，现在的你才最珍贵啊。',
+    '没有不可治愈的伤痛，没有不能结束的沉沦，所有失去的，会以另一种方式归来。',
+    '万物皆有罅隙，那是光透过来的地方。',
+    '一切都会好起来的，真的。'
+];
+var res = //服务器列表
+    "OIer-Meet官方服务器 L1uTongwei https://gitee.com/oier-meet-dev-team https://api.oier-meet.cn";
+var md; //Markdown
