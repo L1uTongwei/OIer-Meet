6fa2c34ef814023fedb7136411d2ccc75eb5edd7
任宇航 <15543677192@163.com>
2023-06-14 08:50:12 +0800

    fixed something (have error now)

--- website/js/body/build_editor.js
+++ website/js/body/build_editor.js
@@ -1,58 +1,56 @@
-export Body {
-	build_editor() {
-		this.editor = new class {
-			#father;
-			#element;
-			object;
-			editor;
-			constructor(fa) {
-				this.#father = fa;
-				this.build();
-			}
-			build() {
-				this.#element = Make.dialog(
-					"editor-dialog",
-					"编辑",
-					Make.element('div', [{
-						"key": "style",
-						"value": "height: 500px;"
-					}], Make.element('div', [{
-							"key": "style",
-							"value": "height: 500px;"
-						},
-						{
-							"key": "id",
-							"value": "editor-container"
-						}
-					]).outerHTML).outerHTML,
-					Make.element('button', [{
-							"key": "class",
-							"value": "mdui-btn mdui-ripple"
-						},
-						{
-							"key": "mdui-dialog-cancel",
-							"value": ""
-						}
-					], '关闭').outerHTML +
-					Make.element('button', [{
-							"key": "class",
-							"value": "mdui-btn mdui-ripple"
-						},
-						{
-							"key": "mdui-dialog-confirm",
-							"value": ""
-						},
-						{
-							"key": "id",
-							"value": "editor-submit"
-						}
-					], '提交').outerHTML
-				);
-				this.#father.appendChild(this.#element);
-				this.object = new mdui.Dialog(this.#element);
-				this.editor = new MarkdownPalettes("#editor-container");
-			}
-		}(Body.#element);
+Body.build_editor = function () {
+    Body.editor = new class {
+        #father;
+        #element;
+        object;
+        editor;
+        constructor(fa) {
+            this.#father = fa;
+            this.build();
+        }
+        build() {
+            this.#element = Make.dialog(
+                "editor-dialog",
+                "编辑",
+                Make.element('div', [{
+                    "key": "style",
+                    "value": "height: 500px;"
+                }], Make.element('div', [{
+                    "key": "style",
+                    "value": "height: 500px;"
+                },
+                {
+                    "key": "id",
+                    "value": "editor-container"
+                }
+                ]).outerHTML).outerHTML,
+                Make.element('button', [{
+                    "key": "class",
+                    "value": "mdui-btn mdui-ripple"
+                },
+                {
+                    "key": "mdui-dialog-cancel",
+                    "value": ""
+                }
+                ], '关闭').outerHTML +
+                Make.element('button', [{
+                    "key": "class",
+                    "value": "mdui-btn mdui-ripple"
+                },
+                {
+                    "key": "mdui-dialog-confirm",
+                    "value": ""
+                },
+                {
+                    "key": "id",
+                    "value": "editor-submit"
+                }
+                ], '提交').outerHTML
+            );
+            this.#father.appendChild(this.#element);
+            this.object = new mdui.Dialog(this.#element);
+            this.editor = new MarkdownPalettes("#editor-container");
+        };
+    };
 
-	};
-}
+};
